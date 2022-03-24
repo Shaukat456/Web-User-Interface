60357cac82357435cfe335e5f5696565f673b4d9
Shaukat Sohail <65353819+Shaukat456@users.noreply.github.com>
2022-03-24 21:38:12 +0500

    KonsulAi

--- src/Components/KonsulAi.jsx
+++ src/Components/KonsulAi.jsx
@@ -3,22 +3,53 @@ function KonsulAi() {
     
         <div className="row">
             <div className="KAI ">
-                <h1 className="display-1">
+                <h1 className="kaih text-light ">
                 KONSUL AI
                 </h1>
 
 
             </div>
-            <div className="row-cols-2 d-flex ">
+            <div className="row-cols-2 d-flex text-light py-5 ">
 
-            <div className="col-6">
+            <div className="col-6 py-5">
            <h1>DESIGN</h1>
+
+
+           <p>SEE HOW IT WORKS</p>
             </div>
             
-            <div className="col-6 text-light">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, tenetur! A possimus repellendus, explicabo vitae cumque quaerat debitis, deserunt porro facilis rem odio rerum? Quibusdam mollitia cumque, voluptatem sequi modi eligendi impedit. Pariatur nam vitae eligendi accusantium consequuntur debitis nulla iusto sequi suscipit, nisi illum voluptatum repellendus cum error sunt illo odit. Nemo quibusdam praesentium excepturi quos quam similique esse. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque iste excepturi esse consectetur mollitia tempore qui necessitatibus accusamus nulla consequuntur facere, dicta illum suscipit, expedita illo aut rerum aspernatur cupiditate culpa magni! Molestiae veritatis modi laudantium corporis! Maiores, excepturi et.
+            <div className="col-6 text-light display-4 py-5">
+                <p>Konsul is an AI-based software for contract analysis. It checks the content of a contract and minimizes legal risks by highlighting negative conditions, regulatory inconsistencies and counterparty edits.
+            <br />
+            <br />
+            <br />
+Thus a legal professional does not need to waste time doing routine contract checking because Konsul AI immediately draws their attention to all the important clauses of the contract and suggests possible solutions.
 
                 </p>
+
+                <hr />
+
+                <div className="d-flex">
+                    
+                
+                <div className="col-lg-6 ">
+                        <h3 className="text-center">70%</h3>
+                       <h5> 
+                           
+                            The time spent on evaluating and negotiating contracts is reduced by 70%
+                           </h5> 
+                </div>
+                    
+                    
+
+
+                <div className="col-lg-6">
+                    <h3 className="text-center">5 min</h3>
+                    <h5>With Konsul AI, contract checking only takes 5 minutes</h5>
+                </div>
+                </div>
+
+
             </div>
             
             </div>
