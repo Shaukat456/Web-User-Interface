470535b0b9ecaf2981779114e2231147ce12024b
Shaukat Sohail <65353819+Shaukat456@users.noreply.github.com>
2022-03-24 20:10:09 +0500

    KonsulAi

--- src/Components/KonsulAi.jsx
+++ src/Components/KonsulAi.jsx
@@ -2,8 +2,25 @@ function KonsulAi() {
     return (  <>
     
         <div className="row">
-            <div className="display-1">
+            <div className="KAI ">
+                <h1 className="display-1">
                 KONSUL AI
+                </h1>
+
+
+            </div>
+            <div className="row-cols-2 d-flex ">
+
+            <div className="col-6">
+           <h1>DESIGN</h1>
+            </div>
+            
+            <div className="col-6 text-light">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, tenetur! A possimus repellendus, explicabo vitae cumque quaerat debitis, deserunt porro facilis rem odio rerum? Quibusdam mollitia cumque, voluptatem sequi modi eligendi impedit. Pariatur nam vitae eligendi accusantium consequuntur debitis nulla iusto sequi suscipit, nisi illum voluptatum repellendus cum error sunt illo odit. Nemo quibusdam praesentium excepturi quos quam similique esse. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque iste excepturi esse consectetur mollitia tempore qui necessitatibus accusamus nulla consequuntur facere, dicta illum suscipit, expedita illo aut rerum aspernatur cupiditate culpa magni! Molestiae veritatis modi laudantium corporis! Maiores, excepturi et.
+
+                </p>
+            </div>
+            
             </div>
         </div>
     
